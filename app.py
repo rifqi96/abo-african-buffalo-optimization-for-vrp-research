2e9c06c2e2248843a86ecb97652da608d9ce6eb1
Rifqi <rifqi96@yahoo.com>
2018-04-30 03:22:06 +0700

    Updates

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ This file creates your application.
 """
 
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from Main import Main
+from Graph import Graph
 
 app = Flask(__name__)
 
@@ -28,6 +30,72 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html')
+
+@app.route('/api/results')
+def results():
+    with app.app_context():
+        data = []
+        main = Main()
+        main.generateResults()
+        for res in main.results:
+            data.append({
+                'buffalo_no':res['buffalo_no'],
+                'total_demands':res['total_demands'],
+                'routes':res['real_nodes'],
+                'graph':{
+                    'name':res['graph'].getLocationNames(),
+                    'coor':res['graph'].getLongLat(),
+                    'node':res['graph'].getNodes(),
+                    'demands':res['graph'].getDemands()
+                },
+                'cost':res['buffalo'].getTotalDistance()
+            })
+        response = jsonify(data)
+        return response
+
+@app.route('/api/results/<int:depot_index>/<float:lp1>/<float:lp2>/<float:speed>/<int:buffalo_size>/<int:trial_size>/<int:bg_not_updating>/<int:max_demands>', methods=['GET','POST'])
+def customResults(depot_index, lp1, lp2, speed, buffalo_size, trial_size, bg_not_updating, max_demands):
+    with app.app_context():
+        data = []
+        main = Main()
+        results = main.generateResults(depot_index, [lp1, lp2], speed, buffalo_size, trial_size, bg_not_updating, max_demands)
+        if results is None:
+            return jsonify(message='Error Happens'), 500
+
+        if results['status'] is False:
+            return jsonify(message=results['message']), 500
+
+        for res in results['data']:
+            data.append({
+                'buffalo_no':res['buffalo_no'],
+                'total_demands':res['total_demands'],
+                'routes':res['real_nodes'],
+                'graph':{
+                    'name':res['graph'].getLocationNames(),
+                    'coor':res['graph'].getLongLat(),
+                    'node':res['graph'].getNodes(),
+                    'demands':res['graph'].getDemands()
+                },
+                'cost':res['buffalo'].getTotalDistance()
+            })
+        response = jsonify(data)
+        return response
+
+@app.route('/api/nodes')
+def getNodes():
+    with app.app_context():
+        graph = Graph()
+        size = len(graph.getLocationNames())
+        data = []
+        for i in xrange(size):
+            data.append({
+                'name':graph.getLocationNames()[i],
+                'coor':graph.getLongLat()[i],
+                'node':graph.getNodes()[i],
+                'demands':graph.getDemands()[i],
+                'distances': graph.getDistance()[i]
+            })
+        return jsonify(data)
 
 
 ###
