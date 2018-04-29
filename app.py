99585154c91d13a00bdad03892859003d0319a45
Rifqi <rifqi96@yahoo.com>
2018-04-30 04:45:55 +0700

    Updated everything

--- app.py
+++ app.py
@@ -1,15 +1,35 @@
-from flask import Flask, request, jsonify, render_template, url_for, redirect
+"""
+Flask Documentation:     http://flask.pocoo.org/docs/
+Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
+Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
+
+This file creates your application.
+"""
+
+import os
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from Main import Main
 from Graph import Graph
-import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'abo-vrp-anya')
+
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
+
+
+###
+# Routing for your application.
+###
 
 @app.route('/')
 def home():
+    """Render website's home page."""
     return render_template('home.html')
-    # return jsonify(message="Welcome to the main app")
+
+
+@app.route('/about/')
+def about():
+    """Render the website's about page."""
+    return render_template('about.html')
 
 @app.route('/api/results')
 def results():
@@ -77,9 +97,34 @@ def getNodes():
             })
         return jsonify(data)
 
+
+###
+# The functions below should be applicable to all Flask apps.
+###
+
+@app.route('/<file_name>.txt')
+def send_text_file(file_name):
+    """Send your static text file."""
+    file_dot_text = file_name + '.txt'
+    return app.send_static_file(file_dot_text)
+
+
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=600'
+    return response
+
+
 @app.errorhandler(404)
-def pageNotFound(error):
-    return jsonify(message='Hi fella, are you lost?'), 404
+def page_not_found(error):
+    """Custom 404 page."""
+    return render_template('404.html'), 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- test.py
+++ test.py
@@ -16,14 +16,24 @@ class TestApp(unittest.TestCase):
         self.assertTrue(rv.data)
         self.assertEqual(rv.status_code, 200)
 
-    def test_results_api_works(self):
-        rv = self.app.get('/api/results')
+    def test_about_page_works(self):
+        rv = self.app.get('/about/')
         self.assertTrue(rv.data)
         self.assertEqual(rv.status_code, 200)
+
+    def test_default_redirecting(self):
+        rv = self.app.get('/about')
+        self.assertEqual(rv.status_code, 301)
 
     def test_404_page(self):
         rv = self.app.get('/i-am-not-found/')
         self.assertEqual(rv.status_code, 404)
+
+    def test_static_text_file_request(self):
+        rv = self.app.get('/robots.txt')
+        self.assertTrue(rv.data)
+        self.assertEqual(rv.status_code, 200)
+        rv.close()
 
 
 if __name__ == '__main__':
